2a5def62078014c1e13b2e7450950f5a37bcaf4e
KeeeTooo <qetevanliparteliani@gmail.com>
2022-09-20 16:26:55 +0400

    first commit

---
+++ lesson 1 .py
@@ -0,0 +1,23 @@
+
+
+
+from operator import is_
+
+
+name = "keti"                        #str
+surname = "liparteliani"             #str
+age=26                               #int
+height=169.9                         #float
+saidentifikacio ="12345678910"       #str
+is_blonde = True                     #Boolean
+knows_programing = False             #boolean
+nikname= "kathlenne"                 #str
+is_motivated = True                  #boolean
+have_cats = 3                        #int
+salary = "15000"                 
+
+print (name+" "+surname+" "+str(age)+" "+str(height)+" "+saidentifikacio+" "+
+str(is_blonde)+" "+str(knows_programing)+" "+nikname+" "+str(is_motivated)+" "+salary+" "+str(have_cats))
+
+
+
