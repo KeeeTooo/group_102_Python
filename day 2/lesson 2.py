850a48b5a352fbdba4318a2f43107757fb6479e9
KeeeTooo <qetevanliparteliani@gmail.com>
2022-09-26 20:51:04 +0400

    lesson 2 davaleba

--- day 2/lesson 2.py
+++ day 2/lesson 2.py
@@ -1,40 +1,48 @@
-print ("this is lesson 2")
-print(len("keti"))
+print("this is lesson 2 homework")
+
+print (len("ketevan liparteliani"))    #დაითვალოს სიმბოლოების ოდენობა
 
 
-my_sentence="""გამარჯობა, მე ვარ ქეთევან ლიპარტელიანი,ჩემი 
-მიზანია ვიყო ყველაზე ყველაზე  """
-print (my_sentence)
+my_sentence="""მე ვარ ქეთევან ლიპარტელიანი, 
+ჩემი მისამართია ანაპის დივიზიის 414"""""
+
+
+if 7>5 :                           #indentation პირობის დროს ორწერტილი და
+ print("sworia")                   #გამოსატანი ინფორმაციის Tab ით დაწერა
 
 
 
-if 10>5:                     #indentation   tab საშუალებით
- print("hello")
+full_name= "ketevan liparteliani"
+print (len(full_name))
+# ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად, დავნომროთ და გამოვიტანოთ
 
+print (full_name[0])   # 0 გამოაქვს პირველი სიმბოლო ბრჭყალების მერე
 
- full_name="nika keshelava"
- print(len(full_name))
- #yvela stringi SegviZlia miviCnioT siad
- print(full_name[0])
+print ("z" in full_name)
 
- print ("Z" in full_name)
+print("k" not in full_name)
 
- print("k" not in full_name)
- print( full_name [2:7])
- print( full_name [2:7:2])   #start:finish:step
-print( full_name [:7])
+print( full_name [2:7])
+
+print( full_name [2:7:2])   #start:finish:step
+
+print( full_name [:6])      #ყველაფერი მეექვსე სიმბოლომდე
+print( full_name [8:])      #ყველაფერი მერვე სიმბოლოდან
 
 full_name = "ketevan liparteliani"
 print (full_name)
 
 print( full_name [-1])
-print( full_name [-4])
+print( full_name [-5])
 print( full_name [-1:-6:-1])
+print( full_name [-1:-6:-2])
 
-#სტრინგბს აქვთ მეთოდები
-#STRING.UPPER()
-#LEN(STRING)
+ #სტრინგბს აქვთ მეთოდები
+ #STRING.UPPER()
+ #string.strip()
+ #LEN(STRING)
 print (full_name.upper())
+print (full_name.lower())
 
 name2="          keti"
 print(name2.strip())
@@ -43,3 +51,4 @@ print(name2.strip())
 print(name2.replace("","#"))
 
 print(name2.replace("i","#"))
+
