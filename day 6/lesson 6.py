8267ab8ff789feb277fa3e14cfc3fc773f7ad38c
KeeeTooo <qetevanliparteliani@gmail.com>
2022-10-12 14:20:56 +0400

    lesson 6

---
+++ day 6/lesson 6.py
@@ -0,0 +1,44 @@
+# for i in range(5):
+#     for j in range(4):
+#         print (str(i)+"nika")
+
+
+# x=0  #sainkrementacio
+# for i in range(5):
+#     for j in range(4):
+#         print (str(x)+"nika")
+#         x+=1
+
+
+
+# total_price=0
+# for char in range(5):
+#     age_of_users=int(input("your age"))
+#     if age_of_users>=3:
+#        total_price+=100
+
+
+
+# print(total_price)
+
+
+
+
+
+
+
+
+
+#list სია
+#ინტ, ფლოატ, ბოოლეან, ლისტ
+
+
+i=0
+x=""
+my_list=["xinkali","mwvadi","lobiani","qababi","wyali"]
+prices=["2","20","15","10","1"]
+while i<len(my_list):
+    pricelist=(my_list[i] +" girs " +prices[i]+ " lari ")
+    x+=pricelist
+    i+=1
+print(x)
