ab5d4a8b8e6ddc785548383f469a06e2bbe33bd9
KeeeTooo <qetevanliparteliani@gmail.com>
2022-10-27 11:10:57 +0400

    lesson 10

---
+++ day 10/lesson 10.py
@@ -0,0 +1,56 @@
+# surname="liparteliani"
+# print(surname[1:5])
+
+
+# def greet():
+#     return "hello world!"
+
+# print(greet)
+
+
+# num=123654
+# arr=[]
+# for i in str(num):
+#     arr.append(int(i))
+# arr.reverse()
+# print(arr)
+
+
+
+# name="ketevan liparteliani"
+
+# print(str(name.split(' ')[0][0]).upper() + '.' + str(name.split(' ')[1][0]).upper())
+
+
+# txt = "ketevan liparteliani"
+
+# x = txt.split()
+# a=str(x[0][0])
+# b=str(x[1][0])
+# print(a.upper()+"."+b.upper())
+
+
+# list =["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]
+# a=-1
+# for i in list:
+#     if i=="needle":
+#         a+=1
+#     else:
+#         a+=0
+# print(a)
+
+my_str="nika 11 keshelava"
+# print(my_str[5]+my_str[6]+"4")
+
+
+#1)დაბეჭდოს 6
+# m_str=[]
+# for i in my_str:
+#     m_str.append(i)
+# print(int(m_str[5])+int(m_str[6])+4)
+
+#2)დაბეჭდოს 15
+m_str=[]
+for i in my_str:
+    m_str.append(i)
+print(int(m_str[5])+int(m_str[6])+13)
