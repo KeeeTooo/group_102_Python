787d37fa7b0603708ee7d2237be1c8772f088684
KeeeTooo <qetevanliparteliani@gmail.com>
2022-11-11 20:24:02 +0400

    lesson 11

---
+++ day 11/lesson 11.py
@@ -0,0 +1,27 @@
+# def my_F(any_string,any_char):
+#     arr=[]
+#     i=0
+#     while i<len(any_string):
+#         if any_string[i]==any_char:
+#             arr.append(i)
+#         i+=1
+#     return arr
+
+# print(my_F("ketevan liparteliani","i"))
+    
+#___________________________________________________________
+
+# x="4868521945434875120652606"
+# bin=[]
+# bin1=""
+# for char in str(x):
+#     if int(char)<5:
+#         bin.append(1)
+#     else:
+#         bin.append(0)
+# print (bin)
+# a=""
+# for char in bin:
+#     a+=str(char)
+# print(a)
+
