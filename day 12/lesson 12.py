1e34de89cb0fdbfb6863063e4a3719dff5d98c20
KeeeTooo <qetevanliparteliani@gmail.com>
2022-11-11 20:24:50 +0400

    lesson 12

---
+++ day 12/lesson 12.py
@@ -0,0 +1,26 @@
+# name="mamuka kargi bikia"
+# print(name.split("k"))
+
+# name="mamuka kargi bichia"
+# name.split()
+
+# n=10
+# i=1
+# arr=[]
+# for i in range(n):
+#     i+=1
+#     arr.append(i)
+# print ((arr)[::-1])
+
+#_______________________________________________________________
+
+
+# arr=[41, 75, 72, 56, 80, 82, 81, 33]
+# my_point=60
+
+# p=0
+# for i in arr:
+#     p+=i
+# print (int(p+my_point)/int(len(arr)+1))
+
+#_________________________________________________________________
